e132471e21f0b893b6eb169db6f26d919aa9f3cf
شهاب القديري <shehabalqudiry@gmail.com>
2022-06-26 01:52:48 +0200

    redirect

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -22,10 +22,11 @@ class RedirectIfAuthenticated
         $guards = empty($guards) ? [null] : $guards;
 
         foreach ($guards as $guard) {
-            if (Auth::guard('admin')->check()) {
-                return redirect(RouteServiceProvider::ADMIN);
-            }
             if (Auth::guard($guard)->check()) {
+                if ($guard == 'admin') {
+                    return redirect(RouteServiceProvider::ADMIN);
+                }
+
                 return redirect(RouteServiceProvider::HOME);
             }
         }
