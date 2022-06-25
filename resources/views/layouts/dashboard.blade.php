dd553953ae947e2d2e811749f42e9a733a504cb8
shehabalqudiry <43017399+shehabalqudiry@users.noreply.github.com>
2022-06-25 21:26:48 +0200

    Update dashboard.blade.php

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -10,8 +10,8 @@
     <meta content="" name="keywords" />
 
     <!-- Favicons -->
-    {{--  <link href="{{ asset('assets') }}/img/favicon.png" rel="icon" />
-    <link href="{{ asset('assets') }}/img/apple-touch-icon.png" rel="apple-touch-icon" />  --}}
+    {{--  <link href="{{ secure_asset('assets') }}/img/favicon.png" rel="icon" />
+    <link href="{{ secure_asset('assets') }}/img/apple-touch-icon.png" rel="apple-touch-icon" />  --}}
 
     <!-- Google Fonts -->
     <!-- <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet"> -->
@@ -19,16 +19,16 @@
     <!-- Vendor CSS Files -->
     {{--  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css"
         integrity="sha384-gXt9imSW0VcJVHezoNQsP+TNrjYXoGcrqBZJpry9zJt8PCQjobwmhMGaDHTASo9N" crossorigin="anonymous" />  --}}
-        <link href="{{ asset('assets') }}/vendor/bootstrap/css/bootstrap{{ app()->getLocale() === 'ar' ? '.rtl' : '' }}.css" rel="stylesheet">
-    <link href="{{ asset('assets') }}/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet" />
-    <link href="{{ asset('assets') }}/vendor/boxicons/css/boxicons.min.css" rel="stylesheet" />
-    <link href="{{ asset('assets') }}/vendor/glightbox/css/glightbox.min.css" rel="stylesheet" />
+        <link href="{{ secure_asset('assets') }}/vendor/bootstrap/css/bootstrap{{ app()->getLocale() === 'ar' ? '.rtl' : '' }}.css" rel="stylesheet">
+    <link href="{{ secure_asset('assets') }}/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet" />
+    <link href="{{ secure_asset('assets') }}/vendor/boxicons/css/boxicons.min.css" rel="stylesheet" />
+    <link href="{{ secure_asset('assets') }}/vendor/glightbox/css/glightbox.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
-    <link href="{{ asset('assets') }}/vendor/swiper/swiper-bundle.min.css" rel="stylesheet" />
+    <link href="{{ secure_asset('assets') }}/vendor/swiper/swiper-bundle.min.css" rel="stylesheet" />
 
     <!-- Template Main CSS File -->
-    {{--  <link id="color" rel="stylesheet" href="{{asset('assets/admin')}}/css/color-5.css">  --}}
-    <link href="{{ asset('assets') }}/css/style.css" rel="stylesheet" />
+    {{--  <link id="color" rel="stylesheet" href="{{secure_asset('assets/admin')}}/css/color-5.css">  --}}
+    <link href="{{ secure_asset('assets') }}/css/style.css" rel="stylesheet" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400&display=swap" rel="stylesheet" />
@@ -50,7 +50,7 @@
         <div class="container-fluid d-flex align-items-center justify-content-between">
             <div class="logo mx-4">
                 <h1><a href="/">Logo</a></h1>
-                {{--  <a href="/"><img src="{{ asset('assets') }}/img/logo.png" alt="" class="img-fluid"></a>  --}}
+                {{--  <a href="/"><img src="{{ secure_asset('assets') }}/img/logo.png" alt="" class="img-fluid"></a>  --}}
             </div>
 
             <nav id="navbar" class="navbar justify-content-between">
@@ -109,18 +109,18 @@
             class="bi bi-arrow-up-short"></i></a>
 
     <!-- Vendor JS Files -->
-    <script src="{{ asset('assets') }}/vendor/purecounter/purecounter.js"></script>
-    <script src="{{ asset('assets') }}/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('assets') }}/vendor/glightbox/js/glightbox.min.js"></script>
-    <script src="{{ asset('assets') }}/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-    <script src="{{ asset('assets') }}/vendor/swiper/swiper-bundle.min.js"></script>
-    <script src="{{ asset('assets') }}/vendor/waypoints/noframework.waypoints.js"></script>
-    <script src="{{ asset('assets') }}/vendor/php-email-form/validate.js"></script>
+    <script src="{{ secure_asset('assets') }}/vendor/purecounter/purecounter.js"></script>
+    <script src="{{ secure_asset('assets') }}/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ secure_asset('assets') }}/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="{{ secure_asset('assets') }}/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="{{ secure_asset('assets') }}/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="{{ secure_asset('assets') }}/vendor/waypoints/noframework.waypoints.js"></script>
+    <script src="{{ secure_asset('assets') }}/vendor/php-email-form/validate.js"></script>
 
     <!-- Template Main JS File -->
-    <script src="{{ asset('assets') }}/js/main.js"></script>
+    <script src="{{ secure_asset('assets') }}/js/main.js"></script>
     @yield('js')
-    <script src="{{ asset('assets') }}/js/repeater.js" type="text/javascript"></script>
+    <script src="{{ secure_asset('assets') }}/js/repeater.js" type="text/javascript"></script>
     <script>
         /* Create Repeater */
         $("#repeater").createRepeater({
