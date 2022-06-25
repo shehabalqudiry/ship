940889aa0fdaa1487e20d2e03a75dee61d655a4b
شهاب القديري <shehabalqudiry@gmail.com>
2022-06-26 01:04:44 +0200

    admin Update asset url

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -7,30 +7,30 @@
     <meta name="description" content="Cuba admin is super flexible, powerful, clean &amp; modern responsive bootstrap 5 admin template with unlimited possibilities.">
     <meta name="keywords" content="admin template, Cuba admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="pixelstrap">
-    <link rel="icon" href="{{asset('assets/admin/images/logo/logo.png')}}" type="image/x-icon">
-    <link rel="shortcut icon" href="{{asset('assets/admin/images/logo/logo.png')}}" type="image/x-icon">
+    <link rel="icon" href="{{secure_asset('assets/admin/images/logo/logo.png')}}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{secure_asset('assets/admin/images/logo/logo.png')}}" type="image/x-icon">
     <title>Dashboard</title>
     <!-- Google font-->
     <link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i&amp;display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900&amp;display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/admin')}}/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="{{secure_asset('assets/admin')}}/css/font-awesome.css">
     <!-- ico-font-->
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/admin')}}/css/vendors/icofont.css">
+    <link rel="stylesheet" type="text/css" href="{{secure_asset('assets/admin')}}/css/vendors/icofont.css">
     <!-- Themify icon-->
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/admin')}}/css/vendors/themify.css">
+    <link rel="stylesheet" type="text/css" href="{{secure_asset('assets/admin')}}/css/vendors/themify.css">
     <!-- Flag icon-->
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/admin')}}/css/vendors/flag-icon.css">
+    <link rel="stylesheet" type="text/css" href="{{secure_asset('assets/admin')}}/css/vendors/flag-icon.css">
     <!-- Feather icon-->
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/admin')}}/css/vendors/feather-icon.css">
+    <link rel="stylesheet" type="text/css" href="{{secure_asset('assets/admin')}}/css/vendors/feather-icon.css">
     <!-- Plugins css start-->
     <!-- Plugins css Ends-->
     <!-- Bootstrap css-->
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/admin')}}/css/vendors/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="{{secure_asset('assets/admin')}}/css/vendors/bootstrap.css">
     <!-- App css-->
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/admin')}}/css/style.css">
-    <link id="color" rel="stylesheet" href="{{asset('assets/admin')}}/css/color-5.css" media="screen">
+    <link rel="stylesheet" type="text/css" href="{{secure_asset('assets/admin')}}/css/style.css">
+    <link id="color" rel="stylesheet" href="{{secure_asset('assets/admin')}}/css/color-5.css" media="screen">
     <!-- Responsive css-->
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/admin')}}/css/responsive.css">
+    <link rel="stylesheet" type="text/css" href="{{secure_asset('assets/admin')}}/css/responsive.css">
   </head>
   <body class="rtl">
     <!-- login page start-->
@@ -39,7 +39,7 @@
         <div class="col-12 p-0">
           <div class="login-card">
             <div>
-              <div><a class="logo" href="index.html"><img class="img-fluid for-light" width="150" src="{{asset('assets/admin/images/logo/logo.png')}}" alt="looginpage"></a></div>
+              <div><a class="logo" href="index.html"><img class="img-fluid for-light" width="150" src="{{secure_asset('assets/admin/images/logo/logo.png')}}" alt="looginpage"></a></div>
               <div class="login-main">
                 @include('admin.layouts.inc.errors')
                 @include('admin.layouts.inc.success')
@@ -87,19 +87,19 @@
         </div>
       </div>
       <!-- latest jquery-->
-      <script src="{{asset('assets/admin')}}/js/jquery-3.5.1.min.js"></script>
+      <script src="{{secure_asset('assets/admin')}}/js/jquery-3.5.1.min.js"></script>
       <!-- Bootstrap js-->
-      <script src="{{asset('assets/admin')}}/js/bootstrap/bootstrap.bundle.min.js"></script>
+      <script src="{{secure_asset('assets/admin')}}/js/bootstrap/bootstrap.bundle.min.js"></script>
       <!-- feather icon js-->
-      <script src="{{asset('assets/admin')}}/js/icons/feather-icon/feather.min.js"></script>
-      <script src="{{asset('assets/admin')}}/js/icons/feather-icon/feather-icon.js"></script>
+      <script src="{{secure_asset('assets/admin')}}/js/icons/feather-icon/feather.min.js"></script>
+      <script src="{{secure_asset('assets/admin')}}/js/icons/feather-icon/feather-icon.js"></script>
       <!-- scrollbar js-->
       <!-- Sidebar jquery-->
-      <script src="{{asset('assets/admin')}}/js/config.js"></script>
+      <script src="{{secure_asset('assets/admin')}}/js/config.js"></script>
       <!-- Plugins JS start-->
       <!-- Plugins JS Ends-->
       <!-- Theme js-->
-      <script src="{{asset('assets/admin')}}/js/script.js"></script>
+      <script src="{{secure_asset('assets/admin')}}/js/script.js"></script>
       <!-- login js-->
       <!-- Plugin used-->
     </div>
